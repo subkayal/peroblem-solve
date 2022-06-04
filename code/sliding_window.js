18f9fe8a59a33a39cf5896d46572b7c0a2e4a534
Subrata <workingsk1@gmail.com>
2022-06-04 23:37:45 +0530

    sliding window program sovled

--- code/sliding_window.js
+++ code/sliding_window.js
@@ -38,7 +38,7 @@ const maxSubarraySum = (arr, num) => {
 console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 3));
 
 
-// minSubArrayLen Solution
+// // minSubArrayLen Solution
 function minSubArrayLen(nums, sum) {
   let total = 0;
   let start = 0;
@@ -46,27 +46,23 @@ function minSubArrayLen(nums, sum) {
   let minLen = Infinity;
  
   while (start < nums.length) {
-    // if current window doesn't add up to the given sum then 
-		// move the window to right
     if(total < sum && end < nums.length){
       total += nums[end];
 			end++;
     }
-    // if current window adds up to at least the sum given then
-		// we can shrink the window 
     else if(total >= sum){
       minLen = Math.min(minLen, end-start);
 			total -= nums[start];
 			start++;
     } 
-    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
     else {
       break;
     }
   }
- 
   return minLen === Infinity ? 0 : minLen;
 }
+
+
 // findLongestSubstring Solution
 function findLongestSubstring(str) {
   let longest = 0;
@@ -78,10 +74,10 @@ function findLongestSubstring(str) {
     if (seen[char]) {
       start = Math.max(start, seen[char]);
     }
-    // index - beginning of substring + 1 (to include current in count)
     longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
     seen[char] = i + 1;
   }
   return longest;
 }
+
+console.log(findLongestSubstring('rithmschool'));
